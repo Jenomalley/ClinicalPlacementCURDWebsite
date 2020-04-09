36013307e78f79f72dae27c25044200ef61980aa
Jennifer <Jennifer@DESKTOP-H9KPS2R.student.litdom.lit.ie>
2020-04-09 17:57:02 +0100

    editing code and debugging coordinator home page

--- app/DatabaseFields.php
+++ app/DatabaseFields.php
@@ -9,7 +9,41 @@ class DatabaseFields
     const StudentTable = "student";
     const StudentId = "student_id";
     const StudentName = "student_name";
-     const StudentYear = "student_year";
-      const Student= "student_name";
+    const StudentYear = "student_year";
+    const StudentGradeId = "student_grades_id";
+    const coordinatorName = "coordinator_name";
+    const PlacementId = "placement_id";
+     const PlacementSupervisorId = "placement_supervisor_id";
+      const CoordinatorId = "coordinator_id";
+       const UserId = "user_id";
+       
+       
+     const LoginTable = "login";
+    const Password = "password";
+    
+     const CoordinatorTable = "student";
+     const CoordinatorId = "coordinator_id";
+     const CoordinatorPhone= "coordinator_phonenum";
+     const CoordinatorEmail = "coordinator_email";
+      
+     
+     const PlacementTable = "placements";
+     const PlacementType= "placement_type";
+     const PlacementName= "placement_name";
+     const PlacementEmail = "placement_email";
+     const PlacementPhone = "placement_phonenum";
+     const PlacementGPS = "placement_gps_coordinates";
+     const PlacementStatus= "placement_status";
+    
+     
+     const SupervisorTable = "supervisor";
+     const PlacementSupervisorName= "placement_supervisor_name";
+     const PlacementEmail = "placement_email";
+     const PlacementPhone = "placement_phonenum";
+     
+     
+    
+	
+      
 
 }
