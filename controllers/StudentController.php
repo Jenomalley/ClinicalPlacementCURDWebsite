3a02ef18f219e46d04f24f746e6898625688c9d1
Jennifer <Jennifer@DESKTOP-H9KPS2R.student.litdom.lit.ie>
2020-04-19 19:27:39 +0100

    editing

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -5,9 +5,10 @@
  * This is the controller for the STUDENT user type
  *
  *
- * 
+ *
  */
-class StudentController extends Controller {
+class StudentController extends Controller
+{
 
     //properties
     private $postArray;     //a copy of the content of the $_POST superglobal array
@@ -20,7 +21,8 @@ class StudentController extends Controller {
 
     //methods
 
-    function __construct($user, $db) { //constructor   
+    function __construct($user, $db)
+    { //constructor
 	parent::__construct($user->getLoggedinState());
 	$this->user = $user;
 	//initialise all the class properties
@@ -35,14 +37,16 @@ class StudentController extends Controller {
 
 //end METHOD - constructor
 
-    public function run() {  // run the controller
+    public function run()
+    {  // run the controller
 	$this->getUserInputs();
 	$this->updateView();
     }
 
 //end METHOD - run the controller
 
-    public function getUserInputs() { // get user input
+    public function getUserInputs()
+    { // get user input
 	//
         //This method is the main interface between the user and the controller.
 	//
@@ -54,9 +58,12 @@ class StudentController extends Controller {
 
 //end METHOD - get user input
 
-    public function updateView() { //update the VIEW based on the users page selection
-	if (isset($this->getArray['pageID'])) { //check if a page id is contained in the URL
-	    switch ($this->getArray['pageID']) {
+    public function updateView()
+    { //update the VIEW based on the users page selection
+	if (isset($this->getArray['pageID']))
+	{ //check if a page id is contained in the URL
+	    switch ($this->getArray['pageID'])
+	    {
 		case "home":
 		    //create objects to generate view content
 		    $home = new Home($this->user, $this->pageTitle, strtoupper($this->getArray['pageID']));
@@ -78,27 +85,27 @@ class StudentController extends Controller {
 		    //update the view
 		    include_once 'views/view_navbar_3_panel.php';  //load the view
 		    break;
-	//	case "messages":
+		    //	case "messages":
 		    //get the model
-		//    $messages = new UnderConstruction($this->user, $this->pageTitle, strtoupper($this->getArray['pageID']));
-		//    $navigation = new Navigation($this->user, $this->getArray['pageID']);
-		//    array_push($this->controllerObjects, $messages, $navigation);
+		    //    $messages = new UnderConstruction($this->user, $this->pageTitle, strtoupper($this->getArray['pageID']));
+		    //    $navigation = new Navigation($this->user, $this->getArray['pageID']);
+		    //    array_push($this->controllerObjects, $messages, $navigation);
 		    //get the content from the model - put into the $data array for the view:
 		    //get the content from the navigation model - put into the $data array for the view:
-		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS                    
-		 //   //get the content from the model - put into the $data array for the view:
-		 //   $data['pageTitle'] = $messages->getPageTitle();
-		 //   $data['pageHeading'] = $messages->getPageHeading();
-		 //   $data['panelHeadRHS'] = $messages->getPanelHead_3(); // A string containing the RHS panel heading/title
-		 //   $data['panelHeadLHS'] = $messages->getPanelHead_1(); // A string containing the LHS panel heading/title
-		 //   $data['panelHeadMID'] = $messages->getPanelHead_2();
-		 //   $data['stringLHS'] = $messages->getPanelContent_1();     // A string intended of the Left Hand Side of the page
-		 //   $data['stringMID'] = $messages->getPanelContent_2();     // A string intended of the Left Hand Side of the page
-		 //   $data['stringRHS'] = $messages->getPanelContent_3();     // A string intended of the Right Hand Side of the page
-		 //   $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
-		    //update the view
-		  //  include_once 'views/view_navbar_3_panel.php'; //load the view
-		   // break;
+		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS
+		//   //get the content from the model - put into the $data array for the view:
+		//   $data['pageTitle'] = $messages->getPageTitle();
+		//   $data['pageHeading'] = $messages->getPageHeading();
+		//   $data['panelHeadRHS'] = $messages->getPanelHead_3(); // A string containing the RHS panel heading/title
+		//   $data['panelHeadLHS'] = $messages->getPanelHead_1(); // A string containing the LHS panel heading/title
+		//   $data['panelHeadMID'] = $messages->getPanelHead_2();
+		//   $data['stringLHS'] = $messages->getPanelContent_1();     // A string intended of the Left Hand Side of the page
+		//   $data['stringMID'] = $messages->getPanelContent_2();     // A string intended of the Left Hand Side of the page
+		//   $data['stringRHS'] = $messages->getPanelContent_3();     // A string intended of the Right Hand Side of the page
+		//   $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
+		//update the view
+		//  include_once 'views/view_navbar_3_panel.php'; //load the view
+		// break;
 
 		case "account":
 		    //create objects to generate view content
@@ -133,9 +140,97 @@ class StudentController extends Controller {
 		    $data['pageTitle'] = $calendar->getPageTitle();
 		    $data['pageHeading'] = $calendar->getPageHeading();
 		    $data['panelHeadRHS'] = $calendar->getPanelHead_2(); // A string containing the RHS panel heading/title
-		    $data['panelHeadLHS'] = $calendar->getPanelHead_1(); // A string containing the LHS panel heading/title 
+		    $data['panelHeadLHS'] = $calendar->getPanelHead_1(); // A string containing the LHS panel heading/title
 		    $data['stringLHS'] = $calendar->getPanelContent_1();     // A string intended of the Left Hand Side of the page
 		    $data['stringRHS'] = $calendar->getPanelContent_2();     // A string intended of the Right Hand Side of the page
+		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
+		    //update the view
+		    include_once 'views/view_navbar_2_panel.php'; //load the view
+		    break;
+
+		case "placementsViewEdit":
+		    //create objects to generate view content
+		    $placements = new placements($this->user, $this->postArray, $this->pageTitle, strtoupper($this->getArray['pageID']), $this->db, $this->getArray['pageID']);
+		    $navigation = new Navigation($this->user, $this->getArray['pageID']);
+		    array_push($this->controllerObjects, $placements, $navigation);
+
+
+		    //get the content from the navigation model - put into the $data array for the view:
+		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS
+		    //get the content from the page content model  - put into the $data array for the view:
+		    $data['pageTitle'] = $placements->getPageTitle();
+		    $data['pageHeading'] = $placements->getPageHeading();
+		    $data['panelHeadRHS'] = $placements->getPanelHead_2(); // A string containing the RHS panel heading/title
+		    $data['panelHeadLHS'] = $placements->getPanelHead_1(); // A string containing the LHS panel heading/title
+		    $data['panelHeadMID'] = $placements->getPanelHead_2();
+		    $data['stringLHS'] = $placements->getPanelContent_1();     // A string intended of the Left Hand Side of the page
+		    $data['stringMID'] = $placements->getPanelContent_2();     // A string intended of the Left Hand Side of the page
+		    $data['stringRHS'] = $placements->getPanelContent_2();     // A string intended of the Right Hand Side of the page
+		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
+		    //update the view
+		    include_once 'views/view_navbar_2_panel.php'; //load the view
+		    break;
+		case "placementsAdd":
+		    //create objects to generate view content
+		    $placements = new Placements($this->user, $this->postArray, $this->pageTitle, strtoupper($this->getArray['pageID']), $this->db, $this->getArray['pageID']);
+		    $navigation = new Navigation($this->user, $this->getArray['pageID']);
+		    array_push($this->controllerObjects, $placements, $navigation);
+
+
+		    //get the content from the navigation model - put into the $data array for the view:
+		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS
+		    //get the content from the page content model  - put into the $data array for the view:
+		    $data['pageTitle'] = $placements->getPageTitle();
+		    $data['pageHeading'] = $placements->getPageHeading();
+		    $data['panelHeadRHS'] = $placements->getPanelHead_2(); // A string containing the RHS panel heading/title
+		    $data['panelHeadLHS'] = $placements->getPanelHead_1(); // A string containing the LHS panel heading/title
+		    $data['panelHeadMID'] = $placements->getPanelHead_2();
+		    $data['stringLHS'] = $placements->getPanelContent_1();     // A string intended of the Left Hand Side of the page
+		    $data['stringMID'] = $placements->getPanelContent_2();     // A string intended of the Left Hand Side of the page
+		    $data['stringRHS'] = $placements->getPanelContent_2();     // A string intended of the Right Hand Side of the page
+		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
+		    //update the view
+		    include_once 'views/view_navbar_2_panel.php'; //load the view
+		    break;
+		case "placementsEdit":
+		    //create objects to generate view content
+		    $placements = new placements($this->user, $this->postArray, $this->pageTitle, strtoupper($this->getArray['pageID']), $this->db, $this->getArray['pageID']);
+		    $navigation = new Navigation($this->user, $this->getArray['pageID']);
+		    array_push($this->controllerObjects, $placements, $navigation);
+
+		    //get the content from the navigation model - put into the $data array for the view:
+		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS
+		    //get the content from the page content model  - put into the $data array for the view:
+		    $data['pageTitle'] = $placements->getPageTitle();
+		    $data['pageHeading'] = $placements->getPageHeading();
+		    $data['panelHeadRHS'] = $placements->getPanelHead_2(); // A string containing the RHS panel heading/title
+		    $data['panelHeadLHS'] = $placements->getPanelHead_1(); // A string containing the LHS panel heading/title
+		    $data['panelHeadMID'] = $placements->getPanelHead_2();
+		    $data['stringRHS'] = $placements->getPanelContent_2();     // A string intended of the Right Hand Side of the page
+		    $data['stringMID'] = $placements->getPanelContent_2();     // A string intended of the Left Hand Side of the page
+		    $data['stringLHS'] = $placements->getPanelContent_1();     // A string intended of the Left Hand Side of the page
+		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
+		    //update the view
+		    include_once 'views/view_navbar_1_panel.php'; //load the view
+		    break;
+		case "placementsDelete":
+		    //create objects to generate view content
+		    $placements = new Modules($this->user, $this->postArray, $this->pageTitle, strtoupper($this->getArray['pageID']), $this->db, $this->getArray['pageID']);
+		    $navigation = new Navigation($this->user, $this->getArray['pageID']);
+		    array_push($this->controllerObjects, $placements, $navigation);
+
+
+		    //get the content from the navigation model - put into the $data array for the view:
+		    $data['menuNav'] = $navigation->getMenuNav();       // an array of menu items and associated URLS
+		    //get the content from the page content model  - put into the $data array for the view:
+		    $data['pageTitle'] = $placements->getPageTitle();
+		    $data['pageHeading'] = $placements->getPageHeading();
+		    $data['panelHeadRHS'] = $placements->getPanelHead_2(); // A string containing the RHS panel heading/title
+		    $data['panelHeadLHS'] = $placements->getPanelHead_1(); // A string containing the LHS panel heading/title
+		    $data['panelHeadMID'] = $placements->getPanelHead_2();
+		    $data['stringLHS'] = $placements->getPanelContent_1();     // A string intended of the Left Hand Side of the page
+		    $data['stringMID'] = $placements->getPanelContent_2();     // A string intended of the Left Hand Side of the page
+		    $data['stringRHS'] = $placements->getPanelContent_2();     // A string intended of the Right Hand Side of the page
 		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
 		    //update the view
 		    include_once 'views/view_navbar_2_panel.php'; //load the view
@@ -163,9 +258,9 @@ class StudentController extends Controller {
 		    $data['stringLHS'] = $home->getPanelContent_1();     // A string intended of the Left Hand Side of the page
 		    $data['stringMID'] = $home->getPanelContent_2();     // A string intended of the Left Hand Side of the page
 		    $data['stringRHS'] = $home->getPanelContent_3();     // A string intended of the Right Hand Side of the page
-		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes                    
+		    $this->viewData = $data;  //put the content array into a class property for diagnostic purposes
 		    //update the view
-		    include_once 'views/view_navbar_3_panel.php'; //load the view                  
+		    include_once 'views/view_navbar_3_panel.php'; //load the view
 
 		    break;
 
@@ -192,7 +287,7 @@ class StudentController extends Controller {
 		    break;
 
 		default:
-		    //no page selected 
+		    //no page selected
 		    //create objects to generate view content
 		    $home = new Home($this->user, $this->pageTitle, strtoupper($this->getArray['pageID']));
 		    $navigation = new Navigation($this->user, $this->getArray['pageID']);
@@ -214,7 +309,9 @@ class StudentController extends Controller {
 		    include_once 'views/view_navbar_3_panel.php';
 		    break;
 	    }
-	} else {//no page selected and NO page ID passed in the URL 
+	}
+	else
+	{//no page selected and NO page ID passed in the URL
 	    //no page selected - default loads HOME page
 	    //create objects to generate view content
 	    $home = new Home($this->user, $this->pageTitle, 'HOME');
@@ -243,7 +340,8 @@ class StudentController extends Controller {
 
 
 
-    public function debug() {   //Diagnostics/debug information - dump the application variables if DEBUG mode is on
+    public function debug()
+    {   //Diagnostics/debug information - dump the application variables if DEBUG mode is on
 	echo '<section>';
 	echo '<!-- The Debug SECTION -->';
 	echo '<div class="container-fluid"   style="background-color: #AAAAAA">'; //outer DIV
@@ -257,9 +355,12 @@ class StudentController extends Controller {
 	echo '<section style="background-color: #BBBBB">';
 	echo '<h4>User Logged in Status:</h4>';
 	echo '<section style="background-color: #FFFFFF">';
-	if ($this->loggedin) {
+	if ($this->loggedin)
+	{
 	    echo 'User Logged In state is TRUE ($loggedin) <br>';
-	} else {
+	}
+	else
+	{
 	    echo 'User Logged In state is FALSE ($loggedin) <br>';
 	}
 	echo '</section>';
@@ -293,7 +394,8 @@ class StudentController extends Controller {
 	echo '<h4>$_GET Arrays</h4>';
 	echo '<section style="background-color: #FFFFFF">';
 	echo '<table class="table table-bordered"><thead><tr><th>KEY</th><th>VALUE</th></tr></thead>';
-	foreach ($_GET as $key => $value) {
+	foreach ($_GET as $key => $value)
+	{
 	    echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
 	}
 	echo '</table>';
@@ -302,7 +404,8 @@ class StudentController extends Controller {
 	echo '<h4>$_POST Array</h4>';
 	echo '<section style="background-color: #FFFFFF">';
 	echo '<table class="table table-bordered"><thead><tr><th>KEY</th><th>VALUE</th></tr></thead>';
-	foreach ($_POST as $key => $value) {
+	foreach ($_POST as $key => $value)
+	{
 	    echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
 	}
 	echo '</table>';
@@ -315,7 +418,7 @@ class StudentController extends Controller {
 	echo '</section>';
     }
 
-// end METHOD - Diagnostics/debug information       
+// end METHOD - Diagnostics/debug information
 }
 
 //end CLASS
